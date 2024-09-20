eb9cd7c244e884b921e9e3ad2dc29ee410d5dd70
Nimisha Rawat <nimisha17rawat@gmail.com>
2024-09-20 19:20:23 +0530

    Add main file

---
+++ js/blog.js
@@ -0,0 +1,54 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.6.0/firebase-app.js";
+import { getDatabase,get,ref } from "https://www.gstatic.com/firebasejs/10.6.0/firebase-database.js";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyBkqkM8vXDsETzdJgMFvq82TG71e7-BVow",
+    authDomain: "blogverse-eae3b.firebaseapp.com",
+    projectId: "blogverse-eae3b",
+    storageBucket: "blogverse-eae3b.appspot.com",
+    messagingSenderId: "816678453249",
+    appId: "1:816678453249:web:f8b164f38df9c00ea2c000"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+
+const db = getDatabase(app)
+
+
+
+
+function getPostData (){
+    const user_ref = ref(db,'post/');
+    get(user_ref).then((snapshot)=>{
+        const data = snapshot.val()
+       
+       let html = "";
+       const table = document.querySelector('.main')
+        for (const key in data){
+          const{title,post_content} = data[key]
+  
+          console.log(post_content);
+         
+         
+           html+= `
+           <div class="post"> 
+               <h2>${title}</h2>
+               <p>
+                 ${post_content}
+               </p>
+           </div>
+          `
+  
+        }
+  table.innerHTML =html
+  
+    })
+  }
+  
+  getPostData()
+  
